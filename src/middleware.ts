7bcefe918a372e30e4790c430193f2f6494128ac
hieutwendee <hieudd@twendeesoft.com>
2024-03-12 16:08:57 +0700

    feat: redirect short link

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import { ROUTER } from "./constant";
+
+export async function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
+
+  const id = url.pathname.slice(3); // remove /e/ from the url
+  console.log("😻 ~ middleware ~ id:", id);
+
+  try {
+    const eventResponse = await fetch(
+      `https://connectx.twendeesoft.com/event/${id}`
+    );
+
+    if (eventResponse.status === 200) {
+      console.warn(`Event with ID "${id}" not found.`);
+      const redirectUrl = new URL(`${url.origin}${ROUTER.EVENT}/${id}`);
+      return NextResponse.redirect(redirectUrl);
+    }
+    return NextResponse.next();
+  } catch (error) {
+    console.error(`Error fetching event:`, error);
+    return NextResponse.next();
+  }
+}
+
+export const config = {
+  matcher: "/e/:id*",
+};
