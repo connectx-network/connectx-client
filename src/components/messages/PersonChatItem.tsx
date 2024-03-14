90d32e5f72c2aef0193f87f1eb3b086c9ce497a7
hocvv <hocvv@twendeesoft.com>
2024-03-14 11:32:07 +0700

    fix UI chat

--- src/components/messages/PersonChatItem.tsx
+++ src/components/messages/PersonChatItem.tsx
@@ -1,6 +1,7 @@
 import { ROUTER } from "@/constant";
 import { Flex, Image, Indicator, Text, Title } from "@mantine/core";
 import Link from "next/link";
+import cx from "clsx";
 
 export type PersonChatItemProps = {
   isDarkMode?: boolean;
@@ -8,7 +9,14 @@ export type PersonChatItemProps = {
 export const PersonChatItem = ({ isDarkMode }: PersonChatItemProps) => {
   return (
     <Link href={`${ROUTER.MESSAGE}/dass`}>
-      <Flex my={16} gap={16} className="cursor-pointer w-ful rounded-lg p-2">
+      <Flex
+        my={16}
+        gap={16}
+        className={cx([
+          "cursor-pointer w-ful rounded-lg p-2",
+          isDarkMode ? "hover:bg-[#2e2e2e]" : "hover:bg-[#f8f9fa]",
+        ])}
+      >
         <Indicator radius="xl" size={10} color="green" className="z-0">
           <Image
             src={"https://picsum.photos/200"}
